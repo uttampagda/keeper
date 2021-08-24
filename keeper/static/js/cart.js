d226ce981cc6179d99e690be0a8cbc44169f2960
uttampagda <72185325+uttampagda89@users.noreply.github.com>
2021-08-24 11:16:01 +0530

    add to cart

--- keeper/static/js/cart.js
+++ keeper/static/js/cart.js
@@ -1,10 +1,11 @@
 function addProduct(){
     let products = [];
-    var product_name = function(value,object) {object.innerHTML= value;};
-    var price = function(value,object) {object.innerHTML= value;};
+    var product_name = document.getElementById("product_name").value;
+    var price = document.getElementById("price").value;
     if(localStorage.getItem('products')){
         products = JSON.parse(localStorage.getItem('products'));
     }
     products.push({'product_name' : product_name + 1, 'price' : price});
     localStorage.setItem('products', JSON.stringify(products));
 }
+
